dfc46d826eb6232b256b1f1f969a6d1136433747
Adi Suryawanshi <adi.suryawanshi2512@gmail.com>
2026-03-15 17:49:59 +0000

    most dependencies are installed in the virtual environment, so we can ignore the fact that they are not tracked by git. The only files that are tracked are the ones that were renamed from pip-25.0.1.dist-info to the corresponding package dist-info or py.typed files.

--- agent e/main.py
+++ agent e/main.py
@@ -1,1 +1,16 @@
-print("Hi im agent e")
+#print("Hi im agent e")
+
+from ollama import chat
+from ollama import ChatResponse
+
+response: ChatResponse = chat(model='mistral:7b', messages=[
+  {
+    'role': 'user',
+    'content': 'Why is the sky blue?',
+  },
+])
+print(response['message']['content'])
+# or access fields directly from the response object
+print(response.message.content)
+
+# E:\.venv
