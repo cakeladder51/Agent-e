fd18bdcf2f06ebc42f0f92e860a1827e8b0de841
Adi Suryawanshi <adi.suryawanshi2512@gmail.com>
2026-03-15 18:05:44 +0000

    fixed import error in agent e/.venv/Lib/site-packages/anyio/_core/_eventloop.py

--- agent e/main.py
+++ agent e/main.py
@@ -1,16 +1,12 @@
 #print("Hi im agent e")
 
 from ollama import chat
-from ollama import ChatResponse
 
-response: ChatResponse = chat(model='mistral:7b', messages=[
-  {
-    'role': 'user',
-    'content': 'Why is the sky blue?',
-  },
-])
-print(response['message']['content'])
-# or access fields directly from the response object
-print(response.message.content)
+stream = chat(
+    model='mistral:7b',
+    messages=[{'role': 'user', 'content': 'Why is the sky blue?'}],
+    stream=True,
+)
 
-# E:\.venv
+for chunk in stream:
+  print(chunk['message']['content'], end='', flush=True)
